2ae4de7581341c5eeef3bf128478e11d901a8b99
Victor Hugo Grabowski Beltramini <vhbeltramini@gmail.com>
2022-09-29 18:59:39 -0300

    Initial Imp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("not implemented yet");
+        System.out.println("not implemented yet");
     }
 
 }
