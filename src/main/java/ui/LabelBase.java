1686d9f9612add1511e58402f87b8c78760b7664
Bruce Vahldick <brucev.br@gmail.com>
2022-10-25 23:39:07 -0300

    logica inicial para abrir o mapa

--- src/main/java/ui/LabelBase.java
+++ src/main/java/ui/LabelBase.java
@@ -3,4 +3,9 @@ package ui;
 import javax.swing.*;
 
 abstract class LabelBase extends JLabel {
+    private int id;
+
+    public LabelBase(int id) {
+        this.id = id;
+    }
 }
--- src/main/java/ui/LabelEstradaCima.java
+++ src/main/java/ui/LabelEstradaCima.java
@@ -1,4 +1,7 @@
 package ui;
 
 public class LabelEstradaCima extends LabelBase{
+    public LabelEstradaCima(int id) {
+        super(id);
+    }
 }
