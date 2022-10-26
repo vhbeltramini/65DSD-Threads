f9a815699281888cc23cdf0429a09d961d33f15c
Bruce Vahldick <brucev.br@gmail.com>
2022-10-26 00:39:17 -0300

    logica inicial para abrir o mapa

--- src/main/java/controller/FileController.java
+++ src/main/java/controller/FileController.java
@@ -26,13 +26,19 @@ public class FileController extends Controller {
     public MalhaViaria criarMapa() {
         FileInterpreter fileInterpreter = new FileInterpreter(file);
         List<Integer> retorno = new ArrayList<>();
+        char c;
         for (int i = 2; i < fileInterpreter.contarLinhas(); i++) {
-            retorno.add(Integer.getInteger(fileInterpreter.lerLinha(i)));
+            String linha = fileInterpreter.lerLinha(i);
+            for (int j = 0; j < linha.length(); j++) {
+                if ((c = linha.charAt(j)) != ' ') {
+                    retorno.add(Integer.parseInt(String.valueOf(c)));
+                }
+            }
         }
-        return new MalhaViaria.MalhaViariaBuilder().altura(Integer.getInteger(fileInterpreter.lerLinha(0)))
-                                                   .largura(Integer.getInteger(fileInterpreter.lerLinha(1)))
-                                                   .celulas(retorno)
-                                                   .build();
-    }
 
+        return new MalhaViaria.MalhaViariaBuilder().altura(Integer.getInteger(fileInterpreter.lerLinha(0)))
+                .largura(Integer.getInteger(fileInterpreter.lerLinha(1)))
+                .celulas(retorno)
+                .build();
+    }
 }
